192d6797776d860133a8b75c6dbef9df2fb7bc45
Mike <mikehsm@outlook.com>
2026-06-04 23:22:05 +0200

    chore: bump elixir to 1.20.0 and OTP to 28.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Clickguard.MixProject do
     [
       app: :clickguard,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [
